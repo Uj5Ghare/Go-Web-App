0e956412994b048f01d1f56812efc9a1e9fded6c
ujwal pachghare <131592212+Uj5Ghare@users.noreply.github.com>
2024-10-27 19:41:58 +0530

    fix: homepage error

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	// Render the home html page from static folder
-	http.ServeFile(w, r, "static/home.html")
+	http.ServeFile(w, r, "static/index.html")
 }
 
 func coursePage(w http.ResponseWriter, r *http.Request) {
